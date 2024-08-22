b93134d4654505a4ca1955c520a8aeae67303af0
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2024-08-22 06:19:21 +0200

    Change the dockerhub repository's name

--- Python_scripts/push_image_to_dockerhub.sh
+++ Python_scripts/push_image_to_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login
-docker tag docker_api_chirpstack simonbeurel/python_application:latest
-docker push simonbeurel/python_application:latest
+docker tag docker_api_chirpstack simonbeurel/python_application_vietnam:latest
+docker push simonbeurel/python_application_vietnam:latest
 
